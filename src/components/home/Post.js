3c831b78b90b2956606d4c10b69b594d8f5ef92f
yegyeom <reallyy1111@gmail.com>
2021-12-14 22:02:10 +0900

    [feat] 홈 화면 하이퍼링크 (작성자, 해시태그) 기능 구현

--- src/components/home/Post.js
+++ src/components/home/Post.js
@@ -1,24 +1,52 @@
-import { useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Pagination } from "swiper";
 import { useNavigate } from 'react-router';
 import moment from 'moment';
+import PostContext from '../../contexts/post';
+import { getPosts } from '../../api/post';
 
 const Post = (props) => {
-  const { pagePostList } = props;
+  const { setPost, setCurrentPage, setTotalLength, setWord, setType } = useContext(PostContext).actions;
+  const { post } = useContext(PostContext).state;
   const navigate = useNavigate();
-  SwiperCore.use([Pagination])
-  const [posts, setPosts] = useState([]);
+  SwiperCore.use([Pagination]);
 
-  useEffect(() => {
-    setPosts(pagePostList);
-  }, [pagePostList]);
-
-  const handleClick = (e) => {
+  const handleEditButtonClick = (e) => {
     navigate('/edit', { state: e.target.value });
   }
 
-  const postList = posts.map((item, idx) => { // 게시글 하나씩 돌면서
+  const handleHashtagClick = async (e) => {
+    const hashtag = e.target.innerText.slice(1);
+    setWord(hashtag);
+    setType('h');
+
+    try {
+      const res = await getPosts(1, hashtag, 'h');
+      setPost(res.data);
+      setCurrentPage(1);
+      setTotalLength(res.data.length);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const handleNickClick = async (e) => {
+    const nick = e.target.innerText.slice(1, e.target.innerText.length - 1);
+    setWord(nick);
+    setType('n');
+
+    try {
+      const res = await getPosts(1, nick, 'n');
+      setPost(res.data);
+      setCurrentPage(1);
+      setTotalLength(res.data.length);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const postList = post.map((item, idx) => { // 게시글 하나씩 돌면서
     const imgList = item.Images.map((img, idx) =>
       <SwiperSlide key={idx}>
         <img alt="img" className="post-img" src={`http://web.expertly.info/~web12/${img.path}`} />
@@ -29,7 +57,7 @@ const Post = (props) => {
     const editContent = item.content.split(/(#[^\s]+)/g).map((v, idx) => {
       if (v.match(/#[^\s]+/)) {
         return (
-          <a href="home" key={idx}>{v}</a>
+          <span value="hashtag" className="post-text-link" onClick={handleHashtagClick} key={idx}>{v}</span>
         );
       }
       return v;
@@ -48,14 +76,14 @@ const Post = (props) => {
           </Swiper>
         </div>
         <div className="post-text">
-          <a href="home" target="_self">[{item.nick}]</a> &nbsp;
+          <span className="post-text-link" onClick={handleNickClick}>[{item.nick}]</span> &nbsp;
           <span>{editContent}</span>
         </div>
         <div className="post-bottom">
           <span >{editDate}</span>
-          {item.UserId === parseInt(sessionStorage.getItem('id')) ? <button className="edit-button" value={item.id} onClick={handleClick}>edit</button> : null}
+          {item.UserId === parseInt(sessionStorage.getItem('id')) ? <button className="edit-button" value={item.id} onClick={handleEditButtonClick}>edit</button> : null}
         </div>
-      </div>
+      </div >
     )
 
   })
